d4f22ee1a3af08820729ce205cca778b6b813d67
Friedrich Romstedt <friedrichromstedt@gmail.com>
2016-08-06 16:12:55 +0200

    Updated 'upy2/__init__.py'
    ``__version__`` and ``__version_tup__`` have been updated.
    
    Import statements of modules which are not yet usable have been
    commented out.
    
    I am not yet decided how to deal with the alpha/beta/rc/production
    state in the version tuple ``__version_tup__``.  Presently this
    "suffix" is just omitted in ``__version_tup__``.  The problem is that
    tuples without suffix, i.e. tuples of production versions, do not
    compare nicely::
    
        >>> (0, 5, 0) >= (0, 5, 0, 'a')
        False
    
    Which complicates comparison.
    
    Probably the suffix-less tuple suffices for our needs, as the most
    important use-case is version comparison of production versions.

--- upy2/__init__.py
+++ upy2/__init__.py
@@ -1,10 +1,10 @@
 # XXX Don't forget to modify the version string in setup.py
-__version__ = '0.4.11b'
-__version_tup__ = (0, 4, 11)
+__version__ = '0.5.0a'
+__version_tup__ = (0, 5, 0)
 
 # Developed since: Jan 2010
 
-# Create the module-scope id generator ...
+# Create the module-scope ID generator ...
 
 from upy2.id_generator import *
 id_generator = IDGenerator()
@@ -12,7 +12,7 @@ id_generator = IDGenerator()
 # Load all the things ...
 
 from upy2.core import *
-from upy2.operators import *
-from upy2.umath import *
-from upy2.averaging import *
-from upy2.linear_regression import *
+#from upy2.operators import *
+#from upy2.umath import *
+#from upy2.averaging import *
+#from upy2.linear_regression import *
