016591c1b374132bdae5bc58df51f53158e6b876
Friedrich Romstedt <friedrichromstedt@gmail.com>
2017-10-25 09:57:14 +0200

    Operator Study: Testing a bug of ancient numpy
    In approx. 2010, I needed to work around a numpy bug regarding the
    second operator argument handed over to the "equal" and "not equal"
    ufunc implementations.
    
    This bug is no longer present in numpy-1.13.3.

---
+++ studies/2017-09-29_2013 Reflected Operators/a02 Equal Bug Test.py
@@ -0,0 +1,28 @@
+""" This Test probes a numpy bug reported in upy several years ago.
+The issue was, that the second operand to the ``equal`` ufunc (and
+likewise to the ``not_equal`` ufunc) was a scalar numpy.object ndarray
+holding the second operand.
+
+The bug apparently is no longer present in numpy-1.13.3 nowadays (Oct
+2017). """
+
+
+import numpy
+
+
+class Reporter(object):
+    def __call__(self, a, b, *args, **kwargs):
+        print b
+
+numpy.set_numeric_ops(equal=Reporter())
+
+
+class X(object):
+    pass
+
+numpy.asarray([1, 2]) == X()
+# output::
+#
+#   <__main__.X object at 0x7f78e0ae7c50>
+#
+# as it should be.
