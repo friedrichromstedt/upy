036c1e60f611187da2fb46d545114970c8fa56b4
Friedrich Romstedt <friedrichromstedt@gmail.com>
2017-12-28 08:07:25 +0100

    uufuncs: Implemented a simple Add uufunc

---
+++ upy2/uufuncs.py
@@ -0,0 +1,23 @@
+from upy2.core import undarray
+
+
+class Add(object):
+    def __call__(self, a, b):
+        derivatives = []
+
+        if isinstance(a, undarray):
+            A = a.nominal
+            derivatives.append((a, 1.0))
+        else:
+            A = a
+
+        if isinstance(b, undarray):
+            B = b.nominal
+            derivatives.append((b, 1.0))
+        else:
+            B = b
+
+        return undarray(
+            nominal=(A + B),
+            derivatives=derivatives,
+        )
