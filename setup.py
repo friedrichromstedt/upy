fcf8cebc9912ccbb2e02a614be105506085f2761
Friedrich Romstedt <friedrichromstedt@gmail.com>
2018-04-13 18:32:45 +0200

    Stepping to v2.0.1a1
    Unfortunately, I tagged some prerelease versions of v2.0.0 with *beta*
    tags instead of the more appropriate *alpha* category.  Everything on
    the way to v2.0 so far is *alpha*.
    
    So I am versioning *this* state of development as "v2.0.1a1" to make
    sure that:
    
    *   The version contains the *alpha* mark;
    *   it is *later* than the v2.0.0 versions.
    
    The codes implemented on their own *are beta* already, but upy2 isn't
    *feature complete*.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class resemples the interface of numpy ndarrays.
 
 distutils.core.setup(
     name='upy',
-    version='2.0.0b2',
+    version='2.0.1a1',
     description='Python Uncertainties',
     long_description=long_description,
     author='Friedrich Romstedt',
