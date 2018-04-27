fd7024da0c1b4754e5ee09978c99c6f09229a02d
Friedrich Romstedt <friedrichromstedt@gmail.com>
2018-04-27 19:55:38 +0200

    Stepping to v2.0.1a4

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class resemples the interface of numpy ndarrays.
 
 distutils.core.setup(
     name='upy',
-    version='2.0.1a2',
+    version='2.0.1a4',
     description='Python Uncertainties',
     long_description=long_description,
     author='Friedrich Romstedt',
