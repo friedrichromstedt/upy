5cc47b82fda08bbbda0f15a26b17cd09c167d710
Friedrich Romstedt <friedrichromstedt@gmail.com>
2020-04-30 17:05:44 +0200

    Updated :file:`LICENSE` and rewritten the *long description*

--- setup.py
+++ setup.py
@@ -1,31 +1,49 @@
 import distutils.core
 
 long_description = \
-r'''`upy <http://friedrichromstedt.github.com/upy/>` is a Python package for 
-handling uncertainties.  It provides a class for storing uncertain arrays or 
-scalars.
+r"""Physical measurements are precise only up to a certain
+*uncertainty*.  Measurement results are thus called *uncertain
+quantities*.
 
-Uncertain arrays can be constructed in three ways:
+When performing calculations with them, the calculation outcomes will
+feature a certain uncertainty as well.  Here, the uncertainty of the
+result will depend on the operands' uncertainty.
 
-1)  From nominal value and a specification of the symmetric error.
-2)  By calculating functions of one or more uncertain arrays.
-3)  From a number of other uncertain arrays, in an array_like format.
+The quanitification of the results of mathematical operations with
+*uncertain operands* is called *uncertainty propagation*.
 
-There is no way to store asymmetric errors.
+Here I am proposing a Python package meant to define uncertain
+operands, to carry out calculations using such operands *with
+uncertainty propagation*, and to provide string representations of any
+*uncertain quantity* involved.
 
-In case (1), all array elements will be independent variables.  The dependence
-on this independent variables is tracked.  The error propagation is always 
-applied to this independent variables.
+String representations of uncertain quantities can be tuned by giving
+the *precision* of the decimal fractions involved based on the
+standard deviation of the uncertainty and the number of standard
+deviations, and conversion of uncertain arrays aligns decimal
+fractions such that the result is well-readable.  ``upy`` uses
+*Algorithmic Differentiation* (also known as *Automatic
+Differentiation*) to track uncertainties through the operations.
+Complex values are supported (both for the nominal value as well as
+for the uncertainty), net uncertainties can be given for real-valued
+quantities.
 
-The functions in case (2) must be known and well-behaved functions, in that
-sense, that their derivatives with respect to their arguments must be 
-accessible given the arguments' nominal values.  This criterion is fulfulled 
-by many real-world functions, and most common functions are implemented.
+For this, ``upy`` provides a new class ``undarray``.  ``undarrays``
+might be used in calculations with other ``undarrays`` or any numeric
+Python or ``numpy`` objects.  A broad range of mathematical operations
+is supported.
 
-Element access, slicing, element assignment is possible.  The uncertain array
-class resemples the interface of numpy ndarrays.
-'''
+``upy`` provides several syntactic conventions adapted to the subject.
+For instance, defining uncertain quantities is possible by writing:
 
+.. code:: python
+
+    uvalue = value +- u(uncertainty)
+
+All functionality is thread safe, and suited both for use in the
+interactive Python shell as well as in programs for numerical
+analysis.
+"""
 
 distutils.core.setup(
     name='upy',
@@ -34,10 +52,13 @@ distutils.core.setup(
     long_description=long_description,
     author='Friedrich Romstedt',
     author_email='friedrichromstedt@gmail.com',
-    url='http://friedrichromstedt.github.com/upy/',
+    #url='http://friedrichromstedt.github.com/upy/',
     packages=[
         'upy2',
         'upy2.typesetting',
+    ],
+    install_requires=[
+        'numpy',
     ],
     classifiers=\
         ['Development Status :: 4 - Beta',
