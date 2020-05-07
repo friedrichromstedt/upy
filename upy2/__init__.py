fcc2993251ed73f074bc697b326a43c14ea5f347
Friedrich Romstedt <friedrichromstedt@gmail.com>
2020-05-07 15:15:11 +0200

    Automatically installing numpy operator overloads
    A difficult long-standing decision has been made: When and how shall the
    numpy operator overloads provided by :mod:`upy2.numpy_operators` be
    installed?  Points to consider:
    
    1.  Maybe the user don't want these overloads to be installed.
    
        This is, however, unlikely.
    
    2.  For ease of use, it is desirable that the overloads are installed
        automaticaly.
    
    3.  Where to call :func:`upy2.install_numpy_operators`?  Care should be
        taken that they aren't installed twice, though it won't break
        anything if they are.  At least three options exist:
    
        a)  Install them when the *lowest level* library depending on the
            operator overloads is imported.
    
            Since there might be more than one library depending on the
            overloads, they might easily be installed twice.
    
        b)  Install them by the *highest level* library using libraries
            needing the overloads to be installed; or let them be installed
            by the *toplevel application*.
    
            This is a white-box appraoch requiring the programmer of the
            toplevel library or the application to know about the
            installation details of the lower-level parts of the program.
    
            Furthermore, the only level guaranteed to be toplevel is the
            application level.  Requiring such applications to install the
            overloads is definitely not a good idea.  For all deeper levels,
            there might be other program components installing the operator
            overloads in parallel.
    
        d)  Use a global variable in :mod:`upy2` or
            :mod:`upy2.numpy_operators` to indicate that the operators have
            been installed.
    
            This is just clumsy; it might even require locking.
    
    4.  Maybe in the future there might another package introduced to choose
        implementations of an operation based on the operands (I call this
        project priops).  Even though priops won't be released soon, I'd
        like to plan such that it would fit in.
    
        When ``priops`` is at some point in time available, ``upy2`` might
        choose to avoid installing the numpy operator overloads, instead
        registering the uufuncs with ``priops`` (which would possibly
        overload numpy operators on itself when numpy is available).
    
        In this appraoch, ``priops`` would become an optional dependency of
        ``upy2``.  When it is available, it would be used.
    
        In this way, the way of using ``upy2`` would not change when
        ``priops`` is available, though it would be a little "magic" that
        ``upy2`` would use ``priops``.  I can live with that.
    
    So alea iacta est: Call
    :func:`upy2.numpy_operators.install_numpy_operators` automatically upon
    importing :mod:`upy2`.  This solves the problem that *high level
    components* should not need to care about low-level details, while still
    the overload will be performed precisely *once*.
    
    Notice that this is a non-backward compatible change:
    :func:`upy2.install_numpy_operators` is not available anymore.  This is
    intentional, as its uses from outside of ``upy2`` shall be eliminated.
    It is strictly speaking permissible to do such change in alpha software.

--- upy2/__init__.py
+++ upy2/__init__.py
@@ -8,7 +8,9 @@ id_generator = IDGenerator()
 # Load all the things ...
 
 from upy2.core import *
-from upy2.numpy_operators import install_numpy_operators
+import upy2.numpy_operators
 #from upy2.umath import *
 #from upy2.averaging import *
 #from upy2.linear_regression import *
+
+upy2.numpy_operators.install_numpy_operators()
