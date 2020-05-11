6c37be092a0cedf89350cf922163e7c552502afd
Friedrich Romstedt <friedrichromstedt@gmail.com>
2020-05-11 17:58:58 +0200

    Test Suite: Began studying ``unittest``
    The files date back to 16 Oct 2016; they haven't been committed yet.

---
+++ test/suite/mod1.py
@@ -0,0 +1,5 @@
+import unittest
+
+import test
+
+unittest.main()
---
+++ test/suite/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(2 + 2, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
