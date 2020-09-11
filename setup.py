6fbf259872f0f4bc136873251ee6080586d274ec
Friedrich Romstedt <friedrichromstedt@gmail.com>
2020-09-11 08:53:10 +0200

    Fixed a problem with the *long description*; stepped the version to v2.0.1b6
    Removed the indentation of the paragraphs comprising the *long
    description* in :file:`/setup.py`.  The former version caused the upload
    to fail::
    
        The description failed to render for 'text/x-rst'.
    
    With the new version the files uploaded fine.

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ of the results depend on the uncertainties of the operands involved.
 The quantification of uncertainties resulting from a mathematical
 operation involving uncertain operands is called *uncertainty
 propagation*.
- From a programmer's point of view, uncertainties might be associated
+From a programmer's point of view, uncertainties might be associated
 with the respective nominal values, such that the propagation of
 uncertainties happens automatically, and existing algorithms can be
 re-used with the *uncertain quantities* as arguments.
- Here I am proposing a Python package to define uncertain operands, to
+Here I am proposing a Python package to define uncertain operands, to
 carry out calculations using such operands *with uncertainty
 propagation*, and to provide string representations of uncertain
 quantities involved.
- Complex numbers can be used to define uncertain quantities; for such
+Complex numbers can be used to define uncertain quantities; for such
 complex-valued uncertainties no net uncertainty can be calculated.
 The objects holding uncertain values have array properties, though
 scalar arrays can be used; elements of an uncertain array behave as if
@@ -27,12 +27,12 @@ given by the number of digits with respect to the uncertainty.
 Furthermore, when typesetting multidimensional arrays with
 uncertainty, the decimal fractions are aligned to improve readability
 of the results.
- ``upy`` uses *algorithmic differentiation* (also known as *automatic
+``upy`` uses *algorithmic differentiation* (also known as *automatic
 differentation*) to track uncertainties through the operations.
 Uncertain values might be used in calculations together with other
 uncertain values as well as with any numeric Python or ``numpy``
 objects.  A range of mathematical functions is supported.
- ``upy`` provides several syntactic conventions appropriate to the
+``upy`` provides several syntactic conventions appropriate to the
 subject.  For instance, defining uncertain quantities is possible by
 writing:
 
@@ -50,7 +50,7 @@ analysis. """
 
 setup(
     name='upy2',
-    version='2.0.1b5',
+    version='2.0.1b6',
     description='A package for uncertainty propagation',
     long_description=long_description,
     long_description_content_type='text/x-rst',
